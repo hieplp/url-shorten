99753f5c80eabe94a66d7172535bd85c7a39565e
Hiệp Lý <hiepphuocly@gmail.com>
2023-05-25 23:03:08 +0700

    [BE] - Update docker-compose file for auth-services

--- docker/build.sh
+++ docker/build.sh
@@ -2,11 +2,11 @@
 
 # List of project directories
 projects=(
-         "gateway-services"
+#         "gateway-services"
          "auth-services"
-         "shorten-services"
-         "user-services"
-         "statistic-services"
+#         "shorten-services"
+#         "user-services"
+#         "statistic-services"
 )
 
 # Loop through the projects and build each one
@@ -18,3 +18,17 @@ do
     echo "Done building $project"
     echo "---------------------------------"
 done
+
+# Loop through the projects and build each one
+echo "---------------------------------"
+echo "Docker building..."
+echo "---------------------------------"
+# shellcheck disable=SC2039
+for project in "${projects[@]}"
+do
+  echo "Docker building $project..."
+  docker build -t "url-shortener/${project}" "../${project}"
+  echo "Done docker building $project"
+  echo "---------------------------------"
+done
+
